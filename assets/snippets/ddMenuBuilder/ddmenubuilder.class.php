a0374a545adcde9a36ca6f60cc383c8679a6e3a8
Ilyas Ronef <ilyas@divandesign.biz>
2015-01-16 11:33:19 +0600

    Initial commit

---
+++ assets/snippets/ddMenuBuilder/ddmenubuilder.class.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * modx ddMenuBuilder class
+ * @version 1.1 (2012-10-17)
+ * 
+ * @uses modx 1.0.6 (Evo)
+ * @uses modx ddTools lib 0.3.
+ * 
+ * @copyright Copyright 2012, DivanDesign
+ * http://www.DivanDesign.ru
+ */
+
+if (!class_exists('ddMenuBuilder')){
+//Подключаем modx.ddTools
+require_once $modx->config['base_path'].'assets/snippets/ddTools/modx.ddtools.class.php';
+
+class ddMenuBuilder {
+	public static $id;
+	public static $table;
+	public static $templates;
+	public static $sortDir;
+	public static $where;
+// 	public $lim;
+	
+	public static function generate($startId, $depth){
+		global $modx;
+		
+// 		$limit = ($depth == self::$depth) ? "LIMIT {self::$lim}" : '';
+		
+		//Получаем все пункты одного уровня
+		$sql = 'SELECT `id`, `menutitle`, `pagetitle`, `published`, `isfolder`
+				FROM '.self::$table.'
+				WHERE `parent` = '.$startId.' AND `deleted` = 0 '.self::$where.'
+				ORDER BY `menuindex` '.self::$sortDir;
+		
+		$dbRes = $modx->db->query($sql);
+		
+		//Если что-то есть
+		if ($modx->db->getRecordCount($dbRes) > 0){
+			//Считаем, что активных по дефолту нету
+			$result = array(); $result['act'] = false;
+			
+			//Проходимся по всем пунктам текущего уровня
+			while ($doc = $modx->db->getRow($dbRes)){
+				//Если вдруг меню у документа не задано, выставим заголовок вместо него
+				if (trim($doc['menutitle']) == '') $doc['menutitle'] = $doc['pagetitle'];
+				
+				//Если это папка (т.е., если есть дочерние)
+				if ($doc['isfolder']){
+					//Получаем детей
+					$children = self::generate($doc['id'], $depth - 1);
+				}
+				
+				//Если надо идти глубже
+				if ($depth > 1){
+					//Получаем дочерние пункты
+					$doc['wrapper'] = $children;
+				}
+				
+				//Если текущий пункт является активным
+				if ($doc['id'] == self::$id) $result['act'] = true;
+				
+				//Если есть дочерние, значит надо использовать какой-то родительский шаблон
+				if ($doc['wrapper']){
+					//Если опубликован, значит надо использовать какой-то опубликованный шаблон
+					if ($doc['published']){
+						//Если текущий пункт является активным
+						if ($doc['id'] == self::$id){
+							//Шаблон активного родительского пункта меню
+							$tpl = self::$templates['parentHere'];
+						//Если не не активный
+						}else{
+							//Если один из дочерних был активным
+							if ($children['act']){
+								//Сообщаем, что что-то активное есть
+								//Шаблон родительского пункта меню, когда активный один из дочерних
+								$tpl = self::$templates['parentActive'];
+							//Если активных дочерних не было
+							}else{
+								//Шаблон родительского пункта меню
+	 							$tpl = self::$templates['parentRow'];
+							}
+						}
+					//Если не опубликован
+					}else{
+						//Если один из дочерних был активным
+						if ($children['act']){
+							//Сообщаем, что что-то активное есть
+							//Шаблон неопубликованного родительского пункта меню, когда активный один из дочерних
+							$tpl = self::$templates['unpubParentActive'];
+						//Если активных дочерних не было
+						}else{
+							//Шаблон неопубликованного родительского пункта меню
+ 							$tpl = self::$templates['unpubParentRow'];
+						}
+					}
+				//Если дочерних нет (отображаемых дочерних)
+				}else{
+					//Если опубликован
+					if ($doc['published']){
+						//Если текущий пункт является активным
+						if ($doc['id'] == self::$id){
+							//Шаблон активного пункта
+							$tpl = self::$templates['here'];
+						//Если активен какой-то из дочерних, не участвующих в визуальном отображении
+						}else if($children['act']){
+							$tpl = self::$templates['active'];
+						//Если не не активный
+						}else{
+							//Шаблон пункта меню
+							$tpl = self::$templates['row'];
+						}
+					}
+				}
+				//Если один из детей (не важно отображаются они или нет, т.е., не зависимо от глубины) активен
+				if ($children['act']) $result['act'] = true;
+				
+				//Подготовим к парсингу
+				$doc['wrapper'] = $doc['wrapper']['str'];
+				//Парсим
+				$result['str'] .= ddTools::parseText($tpl, $doc);
+			}
+			return $result;
+		}else{
+			return false;
+		}
+	}
+}
+}
+?>
---
+++ ddMenuBuilder.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * ddMenuBuilder.php
+ * @version 1.7 (2012-10-17)
+ * 
+ * @desc Строит меню. Идея сниппета в совмещении преимуществ Wayfinder и Ditto при значительном упрощении кода.
+ * 
+ * @uses modx ddTools lib 0.3.
+ * 
+ * Основные параметры:
+ * @param $startId {integer} - Откуда брать. Default: 0.
+ * @param $depth {integer} - Глубина поиска. Default: 1.
+ * @param $sortDir {'ASC', 'DESC'} - Направление сортировки. Default: 'ASC'.
+ * @param $showPublishedOnly {0; 1} - Брать ли только опубликованные документы. Default: 1.
+ * @param $showInMenuOnly {0; 1} - Брать ли только те документы, что надо показывать в меню. Default: 1.
+ * 
+ * Шаблоны:
+ * @param $tplRow {string: chunkName} - Шаблон пункта меню. @required
+ * @param $tplHere {string: chunkName} - Шаблон активного пункта меню. @required
+ * @param $tplActive {string: chunkName} - Шаблон пункта меню, если один из его дочерних документов here, но при этом не отображается в меню (из-за глубины, например). Default: $tplHere.
+ * 
+ * @param $tplParentRow {string: chunkName} - Шаблон пункта меню родителя. Default: $tplRow.
+ * @param $tplParentHere {string: chunkName} - Шаблон активного пункта меню родителя. Default: $tplParentRow.
+ * @param $tplParentActive {string: chunkName} - Шаблон пункта меню родителя, когда дочерний является here. Default: $tplParentHere.
+ * 
+ * @param $tplUnpubParentRow {string: chunkName} - Шаблон пункта меню родителя, если он не опубликован. Default: $tplParentRow.
+ * @param $tplUnpubParentActive {string: chunkName} - Шаблон пункта меню родителя, если он не опубликован и дочерний является активным. Default: $tplParentActive.
+ * 
+ * @param $tplWrap {string: chunkName} - Шаблон внешней обёртки. Доступные плэйсхолдеры: [+wrapper+]. Default: '<ul>[+wrapper+]</ul>'.
+ * 
+ * @copyright 2012, DivanDesign
+ * http://www.DivanDesign.biz
+ */
+
+//Подключаем класс
+require_once $modx->config['base_path'].'assets/snippets/ddMenuBuilder/ddmenubuilder.class.php';
+
+//Откуда брать
+$startId = is_numeric($startId) ? $startId : 0;
+//По умолчанию на 3 уровня
+$depth = (is_numeric($depth)) ? $depth : 1;
+
+//Задаём шаблоны
+$tplRow = isset($tplRow) ? $modx->getChunk($tplRow) : false;
+$tplHere = isset($tplHere) ? $modx->getChunk($tplHere) : false;
+$tplActive = isset($tplActive) ? $modx->getChunk($tplActive) : false;
+
+$tplParentRow = isset($tplParentRow) ? $modx->getChunk($tplParentRow) : false;
+$tplParentHere = isset($tplParentHere) ? $modx->getChunk($tplParentHere) : false;
+$tplParentActive = isset($tplParentActive) ? $modx->getChunk($tplParentActive) : false;
+
+$tplUnpubParentRow = isset($tplUnpubParentRow) ? $modx->getChunk($tplUnpubParentRow) : false;
+$tplUnpubParentActive = isset($tplUnpubParentActive) ? $modx->getChunk($tplUnpubParentActive) : false;
+
+
+ddMenuBuilder::$templates['row'] = $tplRow ? $tplRow : '<li><a href="[~[+id+]~]" title="[+pagetitle+]">[+menutitle+]</a></li>';
+ddMenuBuilder::$templates['here'] = $tplHere ? $tplHere : '<li class="active"><a href="[~[+id+]~]" title="[+pagetitle+]">[+menutitle+]</a></li>';
+ddMenuBuilder::$templates['active'] = $tplActive ? $tplActive : ddMenuBuilder::$templates['here'];
+
+ddMenuBuilder::$templates['parentRow'] = $tplParentRow ? $tplParentRow : '<li><a href="[~[+id+]~]" title="[+pagetitle+]">[+menutitle+]</a><ul>[+wrapper+]</ul></li>';
+//Если не задан шаблон текущего родителя
+if (!$tplParentHere){
+	//Если шаблон родительского пункта был задан, берём его, в противном случае — по умолчанию
+	ddMenuBuilder::$templates['parentHere'] = $tplParentRow ? ddMenuBuilder::$templates['parentRow'] : '<li class="active"><a href="[~[+id+]~]" title="[+pagetitle+]">[+menutitle+]</a><ul>[+wrapper+]</ul></li>';
+}else{
+	ddMenuBuilder::$templates['parentHere'] = $tplParentHere;
+}
+ddMenuBuilder::$templates['parentActive'] = $tplParentActive ? $tplParentActive : ddMenuBuilder::$templates['parentHere'];
+
+ddMenuBuilder::$templates['unpubParentRow'] = $tplUnpubParentRow ? $tplUnpubParentRow : ddMenuBuilder::$templates['parentRow'];
+ddMenuBuilder::$templates['unpubParentActive'] = $tplUnpubParentActive ? $tplUnpubParentActive : ddMenuBuilder::$templates['parentActive'];
+
+$tplWrap = (isset($tplWrap)) ? $modx->getChunk($tplWrap) : '<ul>[+wrapper+]</ul>';
+
+//Получаем id текущего документа
+ddMenuBuilder::$id = $modx->documentIdentifier;
+//Таблицу, в которой лежат страницы
+ddMenuBuilder::$table = $modx->getFullTableName('site_content');
+//Направление сортировки
+ddMenuBuilder::$sortDir = isset($sortDir) ? strtoupper($sortDir) : 'ASC';
+//Условие where для sql
+ddMenuBuilder::$where = '';
+
+//По умолчанию берем только опубликованные документы
+if (!is_numeric($showPublishedOnly) || $showPublishedOnly == 1){
+	ddMenuBuilder::$where .= 'AND `published` = 1 ';
+}
+
+//По умолчанию смотрим только документы, у которых стоит галочка «показывать в меню»
+if (!is_numeric($showInMenuOnly) || $showInMenuOnly == 1){
+	ddMenuBuilder::$where .= 'AND `hidemenu` = 0';
+}
+
+//Генерируем меню
+$result = ddMenuBuilder::generate($startId, $depth);
+
+return ddTools::parseText($tplWrap, array('wrapper' => $result['str']), '[+', '+]');
+?>
